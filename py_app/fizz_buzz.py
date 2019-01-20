72c281ada1544e90c07e9d433e491dfd9bd159cd
Zisaan <mkkhanmaruf@gmail.com>
2019-01-20 09:27:02 +0600

    Finally, the description for this fizz_buzz application has been added.

--- py_app/fizz_buzz.py
+++ py_app/fizz_buzz.py
@@ -1,3 +1,4 @@
+print("Hello programmers, It's the fizz_buzz application which performs to show 'fizz' when you enter a number that divisable by 3 and 'buzz' when your input is divisable by 5. And finally, 'fizz_buzz' will be appeared when your input number is divisable by 3 and 5 both. Let's check it out !!")
 num = int(input("Enter the number --->>  "))
 
 
